f7140981f4d89d038b17ab4ce7bf3e94b9314776
mauricioinaz <mauricio.glez@gmail.com>
2020-12-27 08:59:49 -0500

    day18 part 2

--- day18/solution.py
+++ day18/solution.py
@@ -4,26 +4,26 @@ OPERS = [SUM, MULT]
 
 
 # PART 1
-def get_results(data):
+def get_results(data, adv=False):
     p_input = [list(row.strip()) for row in data]
     results = []
     for line in p_input:
         numbers = [x for x in line if x != ' ']
         numbers = [int(x) if x.isnumeric() else x for x in numbers]
-        numbers = flatten_numbers(numbers)
-        # print(numbers[0])
+        numbers = flatten_numbers(numbers, adv)
+        print(numbers[0])
         results.append(numbers[0])
     return sum(results)
 
 
-def flatten_numbers(numbers):
+def flatten_numbers(numbers, adv):
     if '(' not in numbers:
-        return [new_math(numbers)]
+        return [new_math(numbers, adv)]
     else:
         open = 0
         open_ind = None
         close_ind = None
-        # get matching parenthesis
+        # find matching parenthesis indexes
         for i, n in enumerate(numbers):
             if n == '(':
                 open += 1
@@ -35,15 +35,18 @@ def flatten_numbers(numbers):
                     break
                 else:
                     open -= 1
-        before_section = numbers[:open_ind]
-        section = flatten_numbers(numbers[open_ind+1:close_ind])
-        after_section = numbers[close_ind+1:]
-        return flatten_numbers(before_section + section + after_section)
+        before = numbers[:open_ind]
+        inside = flatten_numbers(numbers[open_ind+1:close_ind], adv)
+        after = numbers[close_ind+1:]
+        return flatten_numbers(before + inside + after, adv)
 
 
-def new_math(numbers):
+# sum or multiply by order of numbers, not priority
+def new_math(numbers, adv):
     if len(numbers) == 1:
         return numbers
+    if adv:
+        return new_adv_math(numbers)
     n = numbers[0]
     op = numbers[1]
     for x in numbers[2:]:
@@ -58,6 +61,27 @@ def new_math(numbers):
 
 
 # PART 2
+# make all sums first, than all multiplications
+def new_adv_math(numbers):
+    n = 0
+    multipliers = []
+    for x in numbers:
+        if x == MULT:
+            multipliers.append(n)
+            n = 0
+        elif type(x) == int:
+            n += x
+    # append last multiplier
+    if n > 0:
+        multipliers.append(n)
+    result = 1
+    for x in multipliers:
+        result *= x
+
+    return result
+
+
+# PART 2
 def part_2(data):
     pass
 
@@ -66,8 +90,8 @@ def main():
     data = list(open('input.txt', 'r'))
     print('Part ONE')
     print(f'{get_results(data)}')
-    # print('Part TWO')
-    # print(f'{part_2(data)}')
+    print('Part TWO')
+    print(f'{get_results(data, True)}')
 
 
 if __name__ == "__main__":
