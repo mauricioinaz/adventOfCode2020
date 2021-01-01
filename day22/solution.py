55b7825ce39a508936b91263ec4fe851fc75ebcc
mauricioinaz <mauricio.glez@gmail.com>
2021-01-01 14:19:03 -0500

    day 22 part 2

--- day22/solution.py
+++ day22/solution.py
@@ -11,22 +11,17 @@ def get_data(data):
 
 
 # PART 1
-def get_score(data):
+def crab_combat(data):
     player_1, player_2 = get_data(data)
     # PLAY
     while len(player_1) > 0 and len(player_2) > 0:
         if player_1[0] > player_2[0]:
-            player_1, player_2 = arrange_deques(player_1, player_2)
+            arrange_deques(player_1, player_2)
         else:
-            player_2, player_1 = arrange_deques(player_2, player_1)
+            arrange_deques(player_2, player_1)
 
-    # GET SCORE
     winner = player_1 if len(player_1) > 0 else player_2
-    winner.reverse()
-    score = 0
-    for x, n in enumerate(winner, 1):
-        score += n*x
-    return score
+    return get_score(winner)
 
 
 def arrange_deques(winner, looser):
@@ -35,10 +30,66 @@ def arrange_deques(winner, looser):
     return winner, looser
 
 
+def get_score(winner):
+    winner.reverse()
+    score = 0
+    for x, n in enumerate(winner, 1):
+        score += n*x
+    return score
+
+
+# PART 2
+def recursive_combat(player_1, player_2, recursive=False):
+    # PLAY
+    previous_rounds = []
+    winner = None
+    while len(player_1) > 0 and len(player_2) > 0:
+        # CHECK PREVIOUS ROUNDS
+        current_round = ([c for c in player_1], [c for c in player_2])
+        if current_round in previous_rounds:
+            winner = player_1
+            if recursive:
+                return True
+            break
+        else:
+            previous_rounds.append(current_round)
+
+        player_1_draw = player_1[0]
+        player_2_draw = player_2[0]
+        # RECURSIVE GAME
+        if player_1_draw < len(player_1) and player_2_draw < len(player_2):
+            if recursive_combat(deque([c for c in list(player_1)[1:player_1_draw+1]]),
+                                deque([c for c in list(player_2)[1:player_2_draw+1]]),
+                                True):
+                # player 1 wins recursive game
+                arrange_deques(player_1, player_2)
+            else:
+                # player 2 wins recursive gane
+                arrange_deques(player_2, player_1)
+
+        # NORMAL GAME
+        elif player_1_draw > player_2_draw:
+            # player 1 wins by high card
+            arrange_deques(player_1, player_2)
+        else:
+            # player 2 wins by high card
+            arrange_deques(player_2, player_1)
+
+    if recursive:
+        return len(player_1) > 0
+
+    if not winner:
+        winner = player_1 if len(player_1) > 0 else player_2
+    return get_score(winner)
+
+
 def main():
     data = list(open('input.txt', 'r'))
     print('Part ONE')
-    print(f'{get_score(data)}')
+    print(f'{crab_combat(data)}')
+    print('Part TWO')
+    player_1, player_2 = get_data(data)
+    print(f'{recursive_combat(player_1, player_2)}')
 
 
 if __name__ == "__main__":
